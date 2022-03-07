8b600d82f60246e6c042d4ccf774b3ca8ff99086
hohno-46466 <hohno.46466@gmail.com>
2022-03-08 07:11:11 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

--- JavaScript/WeeklyAscii/No.1376/BalanceGame/balance_game.js
+++ JavaScript/WeeklyAscii/No.1376/BalanceGame/balance_game.js
@@ -23,5 +23,14 @@ const getPoint = event => {
 
 
 const reset = () => {
+	world.DestroyBody(bar);
+	length =document.getElementById("len").value = 10;
+	bar = setBox(30,30,length2,1,Box2D.Dynamics.b2Body.b2_dynamicBody);
+	for (let i = 0; i < blocks.length; i++) {
+		blocks[i].obj.SetAwake(false);
+		const [x,y] = [(i - Math.floor(i/10)*10)*6+3, 55 - Math.floor(i/10)*5];
+		blocks[i].obj.SetPosition(new b2Vec2(x,y));
+		blocks[i].obj.SetAwake(true);
+	}
 }
 
