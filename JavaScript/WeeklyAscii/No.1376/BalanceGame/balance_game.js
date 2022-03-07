ac7e5f5adbfb594d743e4205723b2049283b9a3c
hohno-46466 <hohno.46466@gmail.com>
2022-03-08 06:52:20 +0900

    updated by @hohno_at_kuimc on hohno-SP6-2019H

---
+++ JavaScript/WeeklyAscii/No.1376/BalanceGame/balance_game.js
@@ -0,0 +1,27 @@
+const bodyDef = new Box2D.Dynamics.b2BodyDef;
+const fixDef = new Box2D.Dynamics.b2FixtureDef;
+const b2Vec2 = Box2D.Common.Math.b2Vec2;
+const blocks = new Array();
+let selectBlock = null;
+let bar, length = 30;
+
+const init = () => {
+}
+
+const setBox = (x,y,w,h,type = Box2D.Dynamics.b2Body.b2_staticBody) => {
+}
+
+const setPolygon = (x,y,n) => {
+}
+
+const update = () => {
+}
+
+
+const getPoint = event => {
+}
+
+
+const reset = () => {
+}
+
