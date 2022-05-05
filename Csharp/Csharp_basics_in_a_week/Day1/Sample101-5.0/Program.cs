183282e162c19d60de3ff4c733d17b81afbdce93
hohno-46466 <hohno.46466@gmail.com>
2022-05-06 03:56:11 +0900

    Updated by @hohno_at_kuimc on hohno-SP6-2019H

---
+++ Csharp/Csharp_basics_in_a_week/Day1/Sample101-5.0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample101_5._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
