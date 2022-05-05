90a56eaa2e133fe2595b8286d7e4e3fa3ce74acc
hohno-46466 <hohno.46466@gmail.com>
2022-05-06 06:52:54 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- Csharp/Csharp_basics_in_a_week/Day1/Sample101-6p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day1/Sample101-6p0/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample101_6p0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	    Console.WriteLine("Hello, World!");
+	}
+    }
+}
