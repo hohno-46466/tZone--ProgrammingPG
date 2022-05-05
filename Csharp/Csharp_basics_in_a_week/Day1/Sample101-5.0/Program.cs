e042670464b1f89bb7d69618321583f09d95c346
hohno-46466 <hohno.46466@gmail.com>
2022-05-06 04:07:31 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- Csharp/Csharp_basics_in_a_week/Day1/Sample101-5.0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day1/Sample101-5.0/Program.cs
@@ -6,7 +6,7 @@ namespace Sample101_5._0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, World!");
         }
     }
 }
