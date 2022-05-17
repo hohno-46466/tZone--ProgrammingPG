296f70ecc041d34cfc0abbc33b451986532b92bb
hohno-46466 <hohno.46466@gmail.com>
2022-05-17 11:01:38 +0900

    Updated by @hohno_at_kuimc on hohno-SP6-2019H

--- Csharp/Csharp_basics_in_a_week/Day2/Sample202-5p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day2/Sample202-5p0/Program.cs
@@ -6,7 +6,17 @@ namespace Sample202_5p0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-        }
+            Console.WriteLine("Hello World! 202 / 5.0");
+            int a;
+            int b = 3;
+            int add, sub;
+            double avg;
+            a = 6;
+            sub = a + b;
+            sub = a - b;
+            avg = (a + b) / 2.0;
+            Console.WriteLine("{0} + {1} = {2}", a, b, add);
+            Console.WriteLine("{0} - {1} = {2}", a, b, add);
+            Console.WriteLine("{0} と {1} の平均値 {2}", a, b, avg);        }
     }
 }
