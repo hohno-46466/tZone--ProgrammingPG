dba920c9f9f37cc7eee1528bf6f6899f1a0fb6b1
hohno-46466 <hohno.46466@gmail.com>
2022-09-17 05:46:33 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

---
+++ Csharp/Csharp_basics_in_a_week/Day2/Sample208-5p0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample208_5p0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Csharp/Csharp_basics_in_a_week/Day2/Sample209-5p0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sample209_5p0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
