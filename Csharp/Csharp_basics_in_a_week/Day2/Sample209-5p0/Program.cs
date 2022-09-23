2680b978cb4c519342454634026ccc243689c212
hohno-46466 <hohno.46466@gmail.com>
2022-09-23 21:26:08 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- Csharp/Csharp_basics_in_a_week/Day2/Sample209-5p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day2/Sample209-5p0/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 
 namespace Sample209_5p0
 {
@@ -6,7 +11,24 @@ namespace Sample209_5p0
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello, Sample209_5p0!");
+
+	    Console.Write("1から3の整数を入力: ");
+	    int num = int.Parse(Console.ReadLine());
+	    if (num == 1)
+	    {
+		Console.WriteLine("one");
+	    } else if (num == 2)
+	    {
+	    	Console.WriteLine("two");
+	    } else if (num == 3)
+	    {
+	    	Console.WriteLine("thrree");
+	    }
+	    else
+	    {
+		Console.WriteLine("不適切な値です。");
+	    }
         }
     }
 }
