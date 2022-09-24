800dee93de578c50d093c5dc9474323c4ecda32b
hohno-46466 <hohno.46466@gmail.com>
2022-09-24 10:47:16 +0900

    updated by @hohno_at_kuimc on hohno-AX3-2020J

--- Csharp/Csharp_basics_in_a_week/Day1/Sample00-6p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day1/Sample00-6p0/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! 6.0");
+
+// Console.WriteLine("Hello, World! 6.0");
+
+using System;
+
+namespace Sample00_5p0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! 6.0 using 5.0 syntax");
+        }
+    }
+}
