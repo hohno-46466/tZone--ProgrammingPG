d8d6b863d76bbccc955dd2b78227433d1b59793f
hohno-46466 <hohno.46466@gmail.com>
2022-10-02 22:56:28 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- Csharp/Csharp_basics_in_a_week/Day4/Sample403-6p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day4/Sample403-6p0/Program.cs
@@ -14,6 +14,14 @@ namespace Sample403
 	static void Main(string[] args)
 	{
 	    Console.WriteLine("Hello, World! (Sample403-6p0)");
+	    Person p1, p2;
+	    p1 = new Person();
+	    p2 = new Person();
+	    p1.Name = "山田太郎";
+	    p1.Age = 19;
+	    p2.SetAgeAndName("佐藤花子", 23);
+	    p1.ShowAgeAndName();
+	    Console.WriteLine("名前:{0} 年齢:{1}", p2.Name, p2.Age);
 	}
     }
 }
