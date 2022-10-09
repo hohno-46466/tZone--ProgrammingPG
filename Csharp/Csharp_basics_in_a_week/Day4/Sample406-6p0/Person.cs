c0d63449a67a9a91a7d0f1b90a1cf6e135b3e7da
hohno-46466 <hohno.46466@gmail.com>
2022-10-09 16:48:30 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- Csharp/Csharp_basics_in_a_week/Day4/Sample406-6p0/Person.cs
+++ Csharp/Csharp_basics_in_a_week/Day4/Sample406-6p0/Person.cs
@@ -4,11 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Sample403
+namespace Sample406
 {
     class Person
     {
 	private string name = "";
+	private int age = 0;
+	public Person() : this("名無し", 0)
+	{
+	    Console.WriteLine("引数なしコンストラクタ");
 	}
-		}
+	public Person(string name, int age)
+	{
+	    this.name = name;
+	    this.age = age;
+	    Console.WriteLine("引数ありコンストラクタ name:{0}  age:{1}", name, age);
 	}
+	public void ShowAgeAndName()
+	{
+	    Console.WriteLine("名前:{0}  年齢:{1}", name, age);
+	}
+	public string Name {
+	    set {name = value; }
+	    get {return name; }
+	}
+	public int Age {
+	    set {age = value; }
+	    get {return age; }
+	}
+    }
+}
--- Csharp/Csharp_basics_in_a_week/Day4/Sample406-6p0/Program.cs
+++ Csharp/Csharp_basics_in_a_week/Day4/Sample406-6p0/Program.cs
@@ -14,6 +14,14 @@ namespace Sample406
 	static void Main(string[] args)
 	{
 	    Console.WriteLine("Hello, World! (Sample406-6p0)");
+	    //
+	    Person p1, p2;
+	    p1 = new Person();
+	    p2 = new Person("山田太郎", 29);
+	    p1.Name = "田中花子";
+	    p1.Age = 18;
+	    p1.ShowAgeAndName();
+	    p2.ShowAgeAndName();
 	}
     }
 }
