e1144682ca3f5a860d7a20b8dc1f964e58d32c69
hohno-46466 <hohno.46466@gmail.com>
2022-10-09 16:11:46 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

---
+++ Csharp/Csharp_basics_in_a_week/Day4/Sample406-6p0/Person.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample403
+{
+    class Person
+    {
+	private string name = "";
+	}
+		}
+	}
