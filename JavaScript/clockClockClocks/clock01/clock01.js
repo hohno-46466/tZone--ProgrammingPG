5c02879ec37ed36da02725dabb0df128407c4b47
hohno-46466 <hohno.46466@gmail.com>
2023-07-13 19:55:40 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- JavaScript/clockClockClocks/clock01/clock01.js
+++ JavaScript/clockClockClocks/clock01/clock01.js
@@ -13,20 +13,53 @@ function setZero3(x){
 }
 
 function showClock() {
-    var nowTime = new Date();
-    var nowHour = setZero2(nowTime.getHours());
-    var nowMin  = setZero2(nowTime.getMinutes());
-    var nowSec  = setZero2(nowTime.getSeconds());
-    var nowMsec = setZero3(nowTime.getMilliseconds());
-    var mesg = "現在時刻："　+ nowHour + ":" + nowMin + ":" + nowSec + "." + nowMsec;
-    document.getElementById("RealtimeClockArea").innerHTML = mesg;
+    var _nowTime = new Date();
+    var _nowHour = setZero2(_nowTime.getHours());
+    var _nowMin  = setZero2(_nowTime.getMinutes());
+    var _nowSec  = setZero2(_nowTime.getSeconds());
+    var _nowMsec = setZero3(_nowTime.getMilliseconds());
+    var mesg = "現在時刻："　+ _nowHour + ":" + _nowMin + ":" + _nowSec + "." + _nowMsec;
+    message.innerHTML = mesg;
 }
 
-function hello() {
-    console.log("Go!");
-    setInterval('showClock()', 1000);
+function startClock() {
+    interval = setInterval('showClock()', 1000);
+    console.log("Go!")
 }
-var nowTime = new Date();
-var msec = 1000 - nowTime.getMilliseconds();
-// console.log(msec);
-setTimeout(hello, msec-10);
+
+function mouseDown() {
+    // console.log("mouseDown()");
+    clearInterval(interval);
+    console.log("clock stopped.");
+
+}
+
+function mouseUp() {
+    // console.log("mouseUp()");
+    console.log("clock restarted.");
+    syncTime();
+}
+
+function buttonClick() {
+    // console.log("buttonClick()");
+}
+
+function syncTime() {
+    var currentTime = new Date();
+    var delay_msec = 1000 - currentTime.getMilliseconds();
+    if (delay_msec < 50) { delay_msec += 1000; }
+    // delay_msec -= 10; // offset
+    console.log("Waiting for " + delay_msec + "msec.");
+    setTimeout(startClock, delay_msec);
+}
+
+var interval;
+var message = document.getElementById("RealtimeClockDisplayArea");
+var button = document.getElementById("button");
+button.addEventListener('mousedown', mouseDown);
+button.addEventListener('mouseup', mouseUp);
+button.addEventListener('click', buttonClick);
+
+showClock();
+syncTime();
+
