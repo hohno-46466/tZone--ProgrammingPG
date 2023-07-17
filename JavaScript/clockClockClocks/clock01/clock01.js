114fb959ad92cb1f05147abb9a1b9b869c212291
hohno-46466 <hohno.46466@gmail.com>
2023-07-17 18:52:27 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- JavaScript/clockClockClocks/clock01/clock01.js
+++ JavaScript/clockClockClocks/clock01/clock01.js
@@ -1,6 +1,7 @@
 // clock01.js
 
-// Last update: Thu Jul 13 21:38:56 JST 2023
+// First version: Thu Jul 13 21:38:56 JST 2023
+// Last update: Mon Jul 17 17:39:07 JST 2023
 
 function setZero2(x){
     var ret;
@@ -18,25 +19,54 @@ function setZero3(x){
 
 function showClock() {
     var _nowTime  = new Date();
+    var _dow3 = new Array("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat");
+
     var _nowYear  = setZero2(_nowTime.getFullYear());
     var _nowMonth = setZero2(_nowTime.getMonth());
-    var _nowdate  = setZero2(_nowTime.getDate());
-    var mesg1 = _nowYear + "-" + _nowMonth + "-" + _nowdate;
+    var _nowDate  = setZero2(_nowTime.getDate());
+    var _nowDow   = _nowTime.getDay();
+    var mesgDate = _nowYear + "-" + _nowMonth + "-" + _nowDate + "(" + _dow3[_nowDow] + ")";
+    
     var _nowHour  = setZero2(_nowTime.getHours());
     var _nowMin   = setZero2(_nowTime.getMinutes());
     var _nowSec   = setZero2(_nowTime.getSeconds());
     var _nowMsec  = setZero3(_nowTime.getMilliseconds());
-    // var mesg2 = _nowHour + ":" + _nowMin + ":" + _nowSec + "." + _nowMsec;
-    var mesg2 = _nowHour + ":" + _nowMin + ":" + _nowSec;
-    var mesg3 = "." + _nowMsec;
-    document.getElementById("RealtimeClockDisplayArea").innerHTML = "現在時刻：" + mesg1 + " " + mesg2;
-    // myDate.innerHTML = mesg1;
-    // myTime.innerHTML = mesg2;
-    // console.log(mesg1);
-    // console.log(mesg2);
-    document.querySelector(".clock-date").innerText = mesg1;
-    document.querySelector(".clock-time").innerText = mesg2;
-    document.querySelector(".clock-time2").innerText = mesg3;
+    var mesgTime1 = _nowHour + ":" + _nowMin + ":" + _nowSec;
+    var mesgTime2 = "." + _nowMsec;
+    var mesgTime = mesgTime1 + mesgTime2;
+
+    var _timeOffset = _nowTime.getTimezoneOffset();
+    var mesgToffset = "UTC"
+    if (_timeOffset > 0) {
+        mesgToffset = mesgToffset + "+" + _timeOffset/60;
+    } else if (_timeOffset < 0) {
+        mesgToffset = mesgToffset + "" + _timeOffset/60;
+    }
+
+    var _nowUTCyear  = setZero2(_nowTime.getUTCFullYear());
+    var _nowUTCmonth = setZero2(_nowTime.getUTCMonth());
+    var _nowUTCdate  = setZero2(_nowTime.getUTCDate());
+    var _nowUTCdow   = _nowTime.getUTCDay();
+    var mesgUTCdate = _nowUTCyear + "-" + _nowUTCmonth + "-" + _nowUTCdate + "(" + _dow3[_nowUTCdow] + ")";
+    
+    var _nowUTChour  = setZero2(_nowTime.getUTCHours());
+    var _nowUTCmin   = setZero2(_nowTime.getUTCMinutes());
+    var _nowUTCsec   = setZero2(_nowTime.getUTCSeconds());
+    var _nowUTCmsec  = setZero3(_nowTime.getUTCMilliseconds());
+    var mesgUTCtime1 = _nowUTChour + ":" + _nowUTCmin + ":" + _nowUTCsec;
+    var mesgUTCtime2 = "." + _nowUTCmsec;
+    var mesgUTCTime = mesgUTCtime1 + mesgUTCtime2;
+
+    document.getElementById("RealtimeClockDisplayArea1").innerHTML = "現在時刻：" + mesgDate + " " + mesgTime1;
+    document.getElementById("RealtimeClockDisplayArea2").innerHTML = "ＵＴＣ　：" + mesgUTCdate + " " + mesgUTCtime1;
+    // myDate.innerHTML = mesgDate;
+    // myTime.innerHTML = mesgTime1;
+    // console.log(mesgDate);
+    // console.log(mesgTime);
+    document.querySelector(".clock-date").innerText = mesgDate;
+    document.querySelector(".clock-time1").innerText = mesgTime1;
+    document.querySelector(".clock-time2").innerText = mesgTime2;
+    document.querySelector(".clock-timezone").innerText = mesgToffset;
 }
 
 function startClock() {
