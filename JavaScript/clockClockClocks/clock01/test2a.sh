fb839a05317c9ba1bfa48dcb8e1b6f7056979503
hohno-46466 <hohno.46466@gmail.com>
2023-08-17 06:54:00 +0900

    updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- JavaScript/clockClockClocks/clock01/test2a.sh
+++ JavaScript/clockClockClocks/clock01/test2a.sh
@@ -9,14 +9,22 @@ NTPSERVER=${3:-"ntp.nict.jp"}
 
 # example: mosquitto_sub -t $TOPIC -h $BROKER | sh ./test2a.sh
 
-echo "#KEYWORD         T1             T2     NTPoffset         T3             T4      --   T2-T1   T4-T3   T3-T2 NTPoffset tDiff  tDiff+NTPoffset"
+MESG="#KEYWORD         T4             T2     NTPoffset         T3             T4      --   T2-T1   T4-T3   T3-T2 NTPoffset tDiff  tDiff+NTPoffset"
+
+echo "$MESG"
+CNT=0
 while [ 1 ] ; do
   read v1 v2 v3 v4 v5
   x=$(date +%s.%3N)
   if [ "x$v1" = "xkeyword2" ]; then
+    CNT=$(expr $CNT + 1)
+    if [ $CNT -ge 10 ]; then
+      CNT=0
+      echo "$MESG"
+    fi
     /bin/echo "$v1 $v2 $v3 $v4 $v5 $x "
   fi
-done | awk '{printf "%s || %7.3lf %7.3lf %7.3lf %7.3lf %7.3lf %7.3lf\n", $0, $3-$2, $6-$5, $5-$3, $4, (($5-$3)-($6-$2))/2, (($5-$3)-($6-$2))/2 + $4; fflush()}'
+done | awk '{if($1~/^#/){print $0} else {printf "%s || %7.3lf %7.3lf %7.3lf %7.3lf %7.3lf %7.3lf\n", $0, $3-$2, $6-$5, $5-$3, $4, (($5-$3)-($6-$2))/2, (($5-$3)-($6-$2))/2 + $4; fflush()}}'
 
 # Note: Since $4(NTPoffset) above is the same value of the result of the ntpddate
 #       (if the value is negative, the server clock(T2 and T3) is ahed of UTC),
