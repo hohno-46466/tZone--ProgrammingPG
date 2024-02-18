719b1d63b63cf12c697a769c7bf16c01c52cd420
hohno-46466 <hohno.46466@gmail.com>
2024-02-19 06:50:26 +0900

    added and updated by @hohno_at_kuimc on hohno-MBP-2022P.local

--- JavaScript/WeeklyAscii/No.1116(006)/spirograph/spirograph.js
+++ JavaScript/WeeklyAscii/No.1116(006)/spirograph/spirograph.js
@@ -18,8 +18,8 @@ let rm, laps;
 let rd;
 let i;
 
-let rad = Math.PI/180;
-let step = 2;
+const rad = Math.PI/180;
+const step = 2;
 
 // let _counter1 = 0, _counter2 = 0;
 // const [width, height] = [800, 600]; // [500, 600];
@@ -34,34 +34,42 @@ function initCanvas() {
 }
 
 function spiro() {
+    // 定円の歯車数、動円の歯車数、描画点の半径
     rc = Number(document.getElementById("rc").value);
     rm = Number(document.getElementById("rm").value);
     rd = Number(document.getElementById("rd").value);
 
+    // 周回数
     laps = rm / gcd(rc, rm);
 
+    // 定円の中心座標
     x0 = Math.floor(Math.random() * (canvas.width - 2 * rc)) + rc;
     y0 = Math.floor(Math.random() * (canvas.height - 2 * rc)) + rc;
 
+    // 描画開始座標
     ox = x0 + rc - rm + rd;
     oy = y0;
 
+    // 描画色
     var r = Math.floor(Math.random() * 256);
     var g = Math.floor(Math.random() * 256);
     var b = Math.floor(Math.random() * 256);
     linecolor = "rgb(" + r + "," + g + "," + b + ")";
 
+    // 描画開始
     i = 0;
-    timer = setInterval(draw, 1);
+    timer = setInterval(draw, 0);
     document.getElementById("status").innerHTML = "drawing... ";
     document.getElementById("draw").disabled = true;
 }
 
 function draw() {
+    // angle, x, y を設定
     var angle = i * rad;
     var x = x0 + (rc - rm) * Math.cos(angle) + rd * Math.cos(((rc - rm) / rm) * angle);
     var y = y0 + (rc - rm) * Math.sin(angle) - rd * Math.sin(((rc - rm) / rm) * angle);
 
+    // (ox, oy) から (x, y) まで線を引く
     context.beginPath();
     context.strokeStyle = linecolor;
     context.moveTo(ox, oy);
@@ -69,16 +77,23 @@ function draw() {
     context.stroke();
     // console.log("debug: " + ox + ", " + oy + ", " + x + ", " + y + " (" + linecolor + ")");
 
+    // ox, oy を更新
     ox = x;
     oy = y;
 
+    // 回転角を更新
     i = i + step;
 
+    // 回転角が 動円の周回数*360 をこえたら描画終了
     if (i > laps * 360) {
+        // 描画終了
         clearInterval(timer);
+        // 終了（complete）を表示
         document.getElementById("status").innerHTML = "complete";
+        // drawボタンを有効にする
         document.getElementById("draw").disabled = false;
     } else {
+        // 現在の周回数を表示
         document.getElementById("status").innerHTML = "drawing... " + Math.floor(i / 360) + "/" +laps;
     }
 }
@@ -89,12 +104,11 @@ function gcd(a, b) {
   } else {
     return gcd(b, a % b);
   }
-
 }
 
 // ---------------------------------------------------------
 
-// // const init = () => {
+// const init = () => {
 //     // キャンバスの取得
 //     canvas = document.getElementById("myCanvas");  // キャンバスの取得
 //     context = canvas.getContext("2d");
@@ -103,32 +117,26 @@ function gcd(a, b) {
 //     // マウスイベントの登録
 //     canvas.addEventListener("click",start_ABC);    // クリックしたとき
 //     canvas.addEventListener("mousemove",move_ABC); // マウスを動かしたとき
-//     //
 //
 //     update();
 // }
 
 // const start_ABC = event => {
-//     //
 //     if ((_counter1 % 1) == 0) {
-// 	console.log("start_ABC : " + _counter1);
+//   　 　console.log("start_ABC : " + _counter1);
 //     }
 //     _counter1++;
 // }
 
 // const move_ABC = event => {
-//     //
 //     if ((_counter2 % 10) == 0) {
-// 	console.log("movet_ABC : " + _counter2);
+// 	    　console.log("movet_ABC : " + _counter2);
 //     }
 //     _counter2++;
-//
 // }
 
 // const update = () => {
-//     //
-//     // window.requestAnimationFrame(update); // アニメーションを実行
+//     window.requestAnimationFrame(update); // アニメーションを実行
 // }
 
-// // ---------------------------------------------------------
-//
+// ---------------------------------------------------------
