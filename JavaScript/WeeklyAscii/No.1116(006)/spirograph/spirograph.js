254a5de88a385462faa0f9659bba6dc15ad17ca5
hohno-46466 <hohno.46466@gmail.com>
2024-03-02 21:34:12 +0900

    added/updated by @hohno_at_kuimc

--- JavaScript/WeeklyAscii/No.1116(006)/spirograph/spirograph.js
+++ JavaScript/WeeklyAscii/No.1116(006)/spirograph/spirograph.js
@@ -1,33 +1,31 @@
 //
-// spirograph.js
+// Spirograph.js
 // (Written in ES2015(ES6))
 //
 // Note:
-// This JS file (spirograph.js) and the corresponding HTML file (spirograph.html) refer to the following pages.
+// This JS file (Spirograph.js) and the corresponding HTML file (Spirograph.html) refer to the following pages.
 // * Weekly Ascii No.1116 (See also: https://ascii.jp/elem/000/001/443/1443136/)
 //
 
-// Last update:
+// FIrst version: 2024-02-20(Tue) 23:17 JST
+// Last update: 2024-03-02(Sat) 15:32 JST / 2024-03-02(Sat) 06:32 UTC
 
-let canvas, context;
-let timer;
-let linecolor;
-let ox, oy;
-let rc, x0, y0;
-let rm, laps;
-let rd;
-let i;
+let canvas, context;      // キャンバス
+let timer;                // タイマー
+let linecolor;            // 描画色
+let ox, oy;               // 描画開始座標
+let rc, x0, y0;           // 定円の半径（歯車数）、中心座標
+let rm, laps;             // 動円の半径（歯車数）、周回数
+let rd;                   // 描画点の半径
+let i;                    // 回転角
 
-const rad = Math.PI/180;
-const step = 2;
-
-// let _counter1 = 0, _counter2 = 0;
-// const [width, height] = [800, 600]; // [500, 600];
+const rad = Math.PI/180;  // radian
+const step = 2;           // 回転角のステップ数
 
 // ---------------------------------------------------------
 
 function initCanvas() {
-    canvas = document.getElementById("spirograph");
+    canvas = document.getElementById("Spirograph");
     context = canvas.getContext("2d");
     canvas.width = window.innerWidth - 20;
     canvas.height = window.innerHeight - 120;
